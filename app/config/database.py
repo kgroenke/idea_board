fc0e4c27b67d14b4555986dc1be6f764a24881a0
Kelsey Groenke <krgroenke@gmail.com>
2016-02-21 11:16:30 -0800

    eited anage.pyu file

--- app/config/database.py
+++ app/config/database.py
@@ -2,20 +2,6 @@
     Database Specific Configuration File
 """
 """ Put Generic Database Configurations here """
-import os
-import psycopg2
-import urlparse
-
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
-
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
 
 class DBConfig(object):
     """ DB_ON must be True to use the DB! """
--- manage.py
+++ manage.py
@@ -6,10 +6,28 @@
 from flask.ext.script import Manager, Server
 from system.init import initialize_app
 import subprocess
+import os
+import psycopg2
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
 
 manager = Manager(initialize_app())
 
-manager.add_command('runserver', Server(host='127.0.0.1'))
+port = int(os.environ.get("PORT", 5000))
+server = Server(host="0.0.0.0", port=port)
+
+manager.add_command('runserver', server)
 
 if __name__ == "__main__":
     manager.run()
