8c895776db957ecd74cda994d6d92a93aa1699f5
Kelsey Groenke <krgroenke@gmail.com>
2016-02-21 15:32:25 -0800

    updating core model

--- system/core/model.py
+++ system/core/model.py
@@ -1,18 +1,18 @@
-# """
-#     System Core Model File
-#
-#     Core Model file that all Models inherit from
-#
-#     Gives a model access to the db object
-# """
-# from flask import Flask
-# from flask import current_app
-# from flask.ext.bcrypt import Bcrypt
-# app = Flask(__name__)
-# from flask.ext.sqlalchemy import SQLAlchemy
-# db = SQLAlchemy(app)
-#
-# class Model(object):
-#     def __init__(self):
-#         self.db = current_app.db
-#         self.bcrypt = Bcrypt(current_app)
+"""
+    System Core Model File
+
+    Core Model file that all Models inherit from
+
+    Gives a model access to the db object
+"""
+from flask import Flask
+from flask import current_app
+from flask.ext.bcrypt import Bcrypt
+app = Flask(__name__)
+from flask.ext.sqlalchemy import SQLAlchemy
+db = SQLAlchemy(app)
+
+class Model(object):
+    def __init__(self):
+        self.db = current_app.db
+        self.bcrypt = Bcrypt(current_app)
