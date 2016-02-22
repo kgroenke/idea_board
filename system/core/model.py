5a5b75857988a6ea100dc867ab10b716d9fce3f1
Kelsey Groenke <krgroenke@gmail.com>
2016-02-22 15:50:21 -0800

    update model

--- system/core/model.py
+++ system/core/model.py
@@ -5,14 +5,13 @@
 
     Gives a model access to the db object
 """
-from flask import Flask
-from flask import current_app
+from flask import Flask, current_app
 from flask.ext.bcrypt import Bcrypt
 app = Flask(__name__)
 from flask.ext.sqlalchemy import SQLAlchemy
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 class Model(object):
     def __init__(self):
-        self.db = db
+        self.db = current_app.db
         self.bcrypt = Bcrypt(current_app)
