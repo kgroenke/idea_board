837b17dd07e4d8400171dba9e1012f22a049373c
Kelsey Groenke <krgroenke@gmail.com>
2016-02-22 15:54:07 -0800

    addempt to give access to db object:

--- system/core/model.py
+++ system/core/model.py
@@ -5,10 +5,10 @@
 
     Gives a model access to the db object
 """
-from flask import Flask, current_app
+from flask import current_app
 from flask.ext.bcrypt import Bcrypt
-app = Flask(__name__)
-from flask.ext.sqlalchemy import SQLAlchemy
+# app = Flask(__name__)
+# from flask.ext.sqlalchemy import SQLAlchemy
 # db = SQLAlchemy(app)
 
 class Model(object):
