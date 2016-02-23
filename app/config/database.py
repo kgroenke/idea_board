c55a8346e52b4bfdeb93d19b8f18eb59756335d0
Kelsey Groenke <krgroenke@gmail.com>
2016-02-23 12:03:54 -0800

    working on db connection

--- app/config/database.py
+++ app/config/database.py
@@ -1,36 +1,51 @@
-# """
-#     Database Specific Configuration File
-# """
-# """ Put Generic Database Configurations here """
-#
-# class DBConfig(object):
-#     """ DB_ON must be True to use the DB! """
-#     DB_ON = True
-#     DB_DRIVER = 'mysql'
-#     DB_ORM = False
-#
-# """ Put Development Specific Configurations here """
-# class DevelopmentDBConfig(DBConfig):
-#     DB_USERNAME = 'root'
-#     DB_PASSWORD = 'root'
-#     DB_DATABASE_NAME = 'idea_board'
-#     DB_HOST = 'localhost'
-#     DB_PORT = 3306
-#     """ unix_socket is used for connecting with MAMP. Take this out if you aren't using MAMP """
-#     DB_OPTIONS = {
-#         'unix_socket': '/Applications/MAMP/tmp/mysql/mysql.sock'
-#     }
-#
-# """ Put Staging Specific Configurations here """
-# class StagingDBConfig(DBConfig):
-#     DB_USERNAME = 'root'
-#     DB_PASSWORD = 'root'
-#     DB_DATABASE_NAME = 'new_red_belt'
-#     DB_HOST = 'localhost'
-#
-# """ Put Production Specific Configurations here """
-# class ProductionDBConfig(DBConfig):
-#     DB_USERNAME = 'root'
-#     DB_PASSWORD = 'root'
-#     DB_DATABASE_NAME = 'new_red_belt'
-#     DB_HOST = 'localhost'
+"""
+    Database Specific Configuration File
+"""
+""" Put Generic Database Configurations here """
+
+import os
+import psycopg2
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+# 
+# conn = psycopg2.connect(
+#     database=url.path[1:],
+#     user=url.username,
+#     password=url.password,
+#     host=url.hostname,
+#     port=url.port
+# )
+
+class DBConfig(object):
+    """ DB_ON must be True to use the DB! """
+    DB_ON = True
+    DB_DRIVER = 'postgres'
+    DB_ORM = False
+
+""" Put Development Specific Configurations here """
+class DevelopmentDBConfig(DBConfig):
+    DB_USERNAME = url.username
+    DB_PASSWORD = url.password
+    DB_DATABASE_NAME = 'idea_board'
+    DB_HOST = url.hostname
+    DB_PORT = url.port
+    """ unix_socket is used for connecting with MAMP. Take this out if you aren't using MAMP """
+    DB_OPTIONS = {
+        'unix_socket': '/Applications/MAMP/tmp/mysql/mysql.sock'
+    }
+
+""" Put Staging Specific Configurations here """
+class StagingDBConfig(DBConfig):
+    DB_USERNAME = url.username
+    DB_PASSWORD = url.password
+    DB_DATABASE_NAME = 'idea_board'
+    DB_HOST = url.hostname
+
+""" Put Production Specific Configurations here """
+class ProductionDBConfig(DBConfig):
+    DB_USERNAME = 'root'
+    DB_PASSWORD = 'root'
+    DB_DATABASE_NAME = 'new_red_belt'
+    DB_HOST = 'localhost'
