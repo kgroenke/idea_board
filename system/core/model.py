fc0f1feff95431cf21465ea499a2b1c16f3d4e24
Kelsey Groenke <krgroenke@gmail.com>
2016-02-24 11:15:32 -0800

    took out comments in core model

--- system/core/model.py
+++ system/core/model.py
@@ -7,9 +7,6 @@
 """
 from flask import current_app
 from flask.ext.bcrypt import Bcrypt
-# app = Flask(__name__)
-# from flask.ext.sqlalchemy import SQLAlchemy
-# db = SQLAlchemy(app)
 
 class Model(object):
     def __init__(self):
